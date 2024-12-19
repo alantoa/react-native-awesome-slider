a78e7b424ede7152a42a906c669aa121d96d7239
alantoa <toacncom@gmail.com>
2024-12-19 21:34:33 +0800

    docs improvements

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,4 +1,6 @@
 import nextra from 'nextra';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
@@ -9,4 +11,50 @@ const withNextra = nextra({
   },
 });
 
-export default withNextra({});
+export default withNextra({
+  webpack(config, options) {
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+
+    config.resolve.alias = Object.assign(config.resolve.alias || {}, {
+      'react-native$': 'react-native-web',
+      'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter$':
+        'react-native-web/dist/vendor/react-native/NativeEventEmitter/RCTDeviceEventEmitter',
+      'react-native/Libraries/vendor/emitter/EventEmitter$':
+        'react-native-web/dist/vendor/react-native/emitter/EventEmitter',
+      'react-native/Libraries/EventEmitter/NativeEventEmitter$':
+        'react-native-web/dist/vendor/react-native/NativeEventEmitter',
+    });
+    config.plugins.push(
+      new options.webpack.DefinePlugin({
+        __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+      })
+    );
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.jsx',
+      '.web.ts',
+      '.web.tsx',
+      ...(config.resolve.extensions || []),
+    ];
+
+    if (!config.plugins) {
+      config.plugins = [];
+    }
+
+    return config;
+  },
+  reactStrictMode: true,
+  transpilePackages: [
+    'react-native',
+    'react-native-web',
+    'expo',
+    'react-native-awesome-slider',
+    'react-native-reanimated',
+    'react-native-gesture-handler',
+  ],
+  experimental: {
+    forceSwcTransforms: true,
+  },
+});
--- docs/src/components/index.tsx
+++ docs/src/components/index.tsx
@@ -1,23 +1,35 @@
-export const IndexPage = () => (
-  <div className="min-h-screen flex flex-col items-center justify-center relative overflow-hidden">
-    <div className="absolute inset-0 bg-gradient-to-r from-orange-100 via-red-100 to-cyan-100">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
-    </div>
+// import { Slider } from 'react-native-awesome-slider';
+// import { useSharedValue } from 'react-native-reanimated';
+// import { Text, View } from 'react-native';
+export const IndexPage = () => {
+  // const progress = useSharedValue(30);
+  // const min = useSharedValue(0);
+  // const max = useSharedValue(100);
 
-    <div className="relative z-10 text-center px-4">
-      <h1 className="text-4xl md:text-6xl font-bold mb-6 text-gray-900">
-        React Native Awesome Slider
-      </h1>
-      <p className="text-md md:text-xl text-gray-600 max-w-2xl mx-auto mb-8">
-        A performant React Native slider built with Reanimated 2 and React
-        Native Gesture Handler.
-      </p>
-      <a
-        href="/docs"
-        className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-black rounded-full hover:bg-gray-800 transition-colors"
-      >
-        Documentation
-      </a>
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center relative overflow-hidden">
+      <div className="absolute inset-0 bg-gradient-to-r from-orange-100 via-red-100 to-cyan-100">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+      </div>
+      {/* <View>
+        <Text>1231</Text>
+      </View> */}
+      {/* <Slider progress={progress} minimumValue={min} maximumValue={max} /> */}
+      <div className="relative z-10 text-center px-4">
+        <h1 className="text-4xl md:text-6xl font-bold mb-6 text-gray-900">
+          React Native Awesome Slider
+        </h1>
+        <p className="text-md md:text-xl text-gray-600 max-w-2xl mx-auto mb-8">
+          A performant React Native slider built with Reanimated 2 and React
+          Native Gesture Handler.
+        </p>
+        <a
+          href="/docs"
+          className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-black rounded-full hover:bg-gray-800 transition-colors"
+        >
+          Documentation
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
